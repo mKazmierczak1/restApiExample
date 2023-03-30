1a5dd59c7e24ae3645162c2e0e6ecaf16aa968a1
Michał Kaźmierczak <mkazmierczak112@gmail.com>
2023-03-30 20:59:24 +0200

    Initial commit

---
+++ src/test/java/com/example/restApiExample/RestApiExampleApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.restApiExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiExampleApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
