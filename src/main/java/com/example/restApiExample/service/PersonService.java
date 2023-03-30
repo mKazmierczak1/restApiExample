be219f05a433f6eb3093f0f607cc4718bf2f52c6
Michał Kaźmierczak <mkazmierczak112@gmail.com>
2023-03-30 21:00:28 +0200

    Created service layer

---
+++ src/main/java/com/example/restApiExample/service/PersonService.java
@@ -0,0 +1,34 @@
+package com.example.restApiExample.service;
+
+import com.example.restApiExample.dao.PersonDao;
+import com.example.restApiExample.model.Person;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PersonService {
+
+  private final PersonDao personDao;
+
+  public Person getById(int id) {
+    return personDao.getById(id).orElseThrow();
+  }
+
+  public List<Person> getAll() {
+    return personDao.getAll();
+  }
+
+  public Person put(Person newPerson) {
+    return personDao.put(newPerson);
+  }
+
+  public Person update(int id, Person newPerson) {
+    return personDao.update(id, newPerson);
+  }
+
+  public Person delete(int id) {
+    return personDao.delete(id);
+  }
+}
